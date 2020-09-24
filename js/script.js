a417f7dc0a5b319aa25944d812554d424db57d48
Alessio Bonetti <alesine87@gmail.com>
2020-09-24 13:21:39 +0200

    flag e star

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// funzione richiamo api film
 function callMovie(movie) {
   $.ajax(
     {
@@ -9,6 +10,7 @@ function callMovie(movie) {
       },
       "method": "GET",
       "success": function(data) {
+        // stampo i dati nel tamplate e appendo
         movieRender (data.results);
       },
       "error": function(err) {
@@ -18,44 +20,113 @@ function callMovie(movie) {
   );
 }
 
-function movieRender(ele) {
+// funzione richiamo api serieTv
+function callSeries(series) {
+  $.ajax(
+    {
+      "url": "https://api.themoviedb.org/3/search/tv" ,
+      "data": {
+        "api_key": "84b45287c2b5227485225f02593b833f",
+        "query": series ,
+        "language": "it-IT"
+      },
+      "method": "GET",
+      "success": function(data) {
+        // stampo i dati nel tamplate e appendo
+        seriesRender(data.results);
+      },
+      "error": function(err) {
+        alert("Errore");
+      }
+    }
+  );
+}
+
+// funzione tamplate e append risultato serietv
+function seriesRender(ele) {
+
   var source = $("#movie-template").html();
   var template = Handlebars.compile(source);
+
   for(var i =0; i<ele.length; i++){
-    var movieVote = takeVote(ele);
-    console.log(movieVote);
+
+    var context = {
+      "title": ele[i].name,
+      "original_title": ele[i].original_name,
+      "language": conversionInStar(ele[i].vote_average),
+      "vote": flagPrinter(ele[i].original_language)
+    };
+
+    var html = template(context);
+
+    $("#series_box").append(html);
+  }
+}
+
+// funzione tamplate e append risultato film
+function movieRender(ele) {
+
+  var source = $("#movie-template").html();
+  var template = Handlebars.compile(source);
+
+  for(var i =0; i<ele.length; i++){
+
     var context = {
       "title": ele[i].title,
       "original_title": ele[i].original_title,
-      "language": ele[i].original_language,
-      "vote": movieVote
+      "language": conversionInStar(ele[i].vote_average),
+      "vote": flagPrinter(ele[i].original_language)
     };
+
     var html = template(context);
+
     $("#movie_box").append(html);
   }
 }
 
+// funzione per svuotare i risultati della ricerca precedente
 function cleanSearch() {
   $("#movie_box").html("");
+  $("#series_box").html("");
   $("#search").val("");
 }
 
-function takeVote(ele) {
-  for (var key in ele) {
-    return ele[key].vote_average;
+// funzione conversione e stampa del numero voto in stelle da 1 a 5
+function conversionInStar(num) {
+  num = Math.ceil(num /2);
+  var string = "";
+
+  for( var i = 0; i<= 5; i++){
+    if(i<=num){
+      string = string + "<i class='fas fa-star'></i>";
+    }else{
+      string = string + "<i class='far fa-star'></i>";
+    }
   }
+  return string;
 }
 
-function conversionNumber(num) {
-  return Math.ceil(num /2);
+// funzione conversione e poi stampa della lingua in bandiera
+function flagPrinter(lang) {
+  var flag = [
+    "it",
+    "en",
+    "ru"
+  ];
+  if(flag.includes(lang)){
+    return "<img class='language_img' src='img/"+lang+".svg'>";
+  }
+  return lang;
 }
 
+// Documento
 $(document).ready(
   function(){
     $(".search_button").click(
       function(){
         var searchedMovie = $("#search").val();
         callMovie (searchedMovie);
+        callSeries(searchedMovie);
         cleanSearch ();
       }
     );
@@ -65,6 +136,7 @@ $(document).ready(
         if(event.which == 13){
           var searchedMovie = $("#search").val();
           callMovie (searchedMovie);
+          callSeries(searchedMovie);
           cleanSearch ();
         }
       }
